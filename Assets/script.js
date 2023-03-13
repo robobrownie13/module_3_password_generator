d2f86b511238498b5a55a210babdfad6895854ec
robobrownie13 <86945332+robobrownie13@users.noreply.github.com>
2023-03-13 10:53:42 -0500

    functioning generator code and updated README

--- Assets/script.js
+++ Assets/script.js
@@ -1,32 +1,60 @@
-// Assignment code here
+// Accesible global variables
+var lengthInput = 0;
+var options = [];
 var pLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p",
 "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var pUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", 
 "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numerals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var pSpecial = ["!", "@", "#", "$", "%","^", "&", "*", "?"]
+var pNumerals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var pSpecial = ["!", "@", "#", "$", "%","^", "&", "*", "?", "/", "'", "(",")","-","+"]
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var lengthInput = window.prompt("Enter your desired password length between 8 and 128 characters: ")
-if (!lengthInput || !lengthInput.isInteger()) {
-  return;
-} else if (lengthInput > 8 || lengthInput < 128) {
-  window.alert("Please read instructions properly")
-}
-// Write password to the #password input
+
+//initiates other functions and displays final password
 function writePassword() {
-  var password = generatePassword();
+  var queries = promptUser();
+  var password = generatePassword(options);
   var passwordText = document.querySelector("#password");
-  /*------------------------------------------------------*/
-
-  /*------------------------------------------------------*/
-  var upperInput = window.confirm()
-
-
   passwordText.value = password;
 
 }
+// gathers desired information from user
+function promptUser() {
+  lengthInput = parseInt(prompt("Enter your desired password length between 8 and 128 characters: "));
+  
+  if(!lengthInput || lengthInput < 8 || lengthInput > 128){
+    lengthInput = alert("Incorrect input. Please try again.");
+    return false;
+  };
 
+  if (confirm(
+    "Do you want to include upper case letters?\nOk for Yes. Cancel for No."
+  )) {options = options.concat(pUpper)};
+
+  if (confirm(
+    "Do you want to include lower case letters?\nOk for Yes. Cancel for No."
+  )) {options = options.concat(pLower)};
+
+  if(confirm(
+    "Do you want to include numbers?\nOk for Yes. Cancel for No."
+  )); {options = options.concat(pNumerals)}
+
+  if(confirm(
+    "Do you want to include special characters?\nOk for Yes. Cancel for No."
+   )); {options = options.concat(pSpecial)}
+  // console.log(options)
+};
+
+// randomizes character options
+function generatePassword() {
+  var passwordArray = [];
+  for(var i = 0; i < lengthInput; i++) {
+    var pChar= options[Math.floor(Math.random()*options.length)];
+    passwordArray.push(pChar)
+  }
+  // console.log(passwordArray);
+  return passwordArray.join("");
+};
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Password Generator Starter Code
+/*module_3_password_generator*/
+# Challenge 3: Password Generator
+
+## User Story
+
+
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+
+## Mock-Up
+
+<img src="./Assets/03-javascript-homework-demo.png" alt="password generator layout">
