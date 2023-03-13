639bd2fbcf4235e62662e86463d736cc9d50ad5f
robobrownie13 <86945332+robobrownie13@users.noreply.github.com>
2023-03-13 13:10:03 -0500

    Added semicolons to the ends of global variables

--- Assets/script.js
+++ Assets/script.js
@@ -2,11 +2,11 @@
 var lengthInput = 0;
 var options = [];
 var pLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var pUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", 
-"Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var pNumerals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var pSpecial = ["!", "@", "#", "$", "%","^", "&", "*", "?", "/", "'", "(",")","-","+"]
+"Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var pNumerals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var pSpecial = ["!", "@", "#", "$", "%","^", "&", "*", "?", "/", "'", "(",")","-","+"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
